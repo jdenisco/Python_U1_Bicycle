a403619c1d670b445839ceb95cfefc43fa9b87f5
James Denisco <james.denisco@genesys.com>
2014-10-09 22:12:04 -0400

    More debug messages

--- bike-test.py
+++ bike-test.py
@@ -14,7 +14,6 @@
 Description:
 """
 
-import json
 import bike_class as B
 
 bike1 = B.Bicycle('Huffy')
@@ -24,7 +23,7 @@ bike2 = B.Bicycle('schwinn')
 bike2.weight(13)
 bike2.cost_to_produce(1450)
 
-mystore = B.Bike_Shops('Jims Bike Store', 1)
+mystore = B.BikeShops('Jims Bike Store', 1)
 mystore.cost_to_produce(450)
 print ('Bike model %s ' % bike1.model_name)
 print ('Bike weight %s lbs' % bike1.weight)
@@ -32,17 +31,24 @@ print ('Cost to build this bike %s ' % bike1.cost_to_produce)
 print ('Mystore cost to make %s ' % mystore.cost_to_produce)
 print ('Mystore saleprice %s ' % mystore.saleprice(20))
 print ('Mystore profit %s ' % mystore.profit())
+print('------------------')
+print('adding bikes to inventory')
 mystore.add_bike(bike1)
 mystore.add_bike(bike2)
 
+print('Print inventory')
 for a in mystore.list_inventory():
     print a.model_name
 
 print('------------------')
+print('Sold a bike')
 mystore.sold_bike(bike2)
 
+print('Print new inventory')
 for c in mystore.list_inventory():
     print c.model_name
 
+print('------------------')
+# import json
 # print(json.dumps(mystore.__dict__))
 # print(dir(mystore))
